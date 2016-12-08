bbc73399ceff30a9c9557fe8639c5b76c676d3b1
Viacheslav <namesakeakzet@gmail.com>
2016-12-08 16:09:04 +0300

    reorginizing file structure of d02 and add d03 files without implementation

---
+++ Java/d03/src/com/accessor/File.java
@@ -0,0 +1,11 @@
+package com.accessor;
+
+import java.io.File;
+
+ public class File{
+ 	//релизовать создание файла
+ 	public File () {//конструкто по умолчанию
+
+ 	}
+
+ }
---
+++ Java/d03/src/com/accessor/User.java
@@ -0,0 +1,5 @@
+package com.accessor;
+
+public class User{
+
+}
