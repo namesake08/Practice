51cc16b6c7dde2b06277216085b4ea9414c42161
Viacheslav <namesakeakzet@gmail.com>
2016-12-12 23:13:33 +0300

    add User implementation

--- Java/d03/src/com/accessor/User.java
+++ Java/d03/src/com/accessor/User.java
@@ -1,5 +1,31 @@
 package com.accessor;
 
-public class User{
+public class User{	
+	private String name;
+	private String secondName;
 
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setSecondName(String secondName){
+		this.secondName = secondName;
+	}
+
+	public String getSecondName(){
+		return secondName;
+	}
+
+	User(String name){
+		this.name = name;
+	}
+
+	User(String name, String secondName){
+		this(name);
+		this.secondName;
+	}
 }
