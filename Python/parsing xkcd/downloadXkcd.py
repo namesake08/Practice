a02b3e06aea24dc5cab1720d198ed238d4917175
Viacheslav Kochergin <namesakeakzet@gmail.com>
2017-06-15 11:53:39 +0300

    Finishing xkcd parser and start maplt srcipt

--- Python/parsing xkcd/downloadXkcd.py
+++ Python/parsing xkcd/downloadXkcd.py
@@ -14,12 +14,26 @@ while not url.endswith('#'):
 
 	soup = bs4.BeautifulSoup(res.text)
 
-#TODO: URL-адрес изображения комикса.
+	#Поиск URL-адреса изображения комикса.
+	comicElem = soup.select('#comic img')
+	if comicElem == []:
+		print('Не удалось найти изображение комикса.')
+	else:
+		comicUrl = comicElem[0].get('scr')
+		#Загрузить изображение.
+		print('Загружается изображение %s...' % (comicUrl))
+		res = requests.get(comicUrl)
+		res.raise_for_status()
 
-#TODO: Загрузить изображение.
+		#Сохранение ихображения в папке ./xkcd.
+		imageFile = open(os.path.join('xkcd', os.path.
+						basename(comicUrl)), 'wb')
+		for chunk in res.iter_content(100000):
+			imageFile.write(chunk)
+		imageFile.close()
 
-#TODO: Сохранить ихображение в папке ./xkcd.
-
-#TODO: Получить URL-адрес кнопки Prev.
+	#Получение URL-адреса кнопки Prev.
+	prevLink = soup.select('a[rel="prev"]')[0]
+	url = 'http://xkcd.com' + prevLink.get('href')
 
 print('Готово.')
