53f5d81049df488988ed63473ad496adb94d9e13
namesake <namesakeakzet@gmail.com>
2019-09-23 06:41:29 +0300

    bubble_sort cycle is fixed

--- C/SimpleExamples/BubbleSort.c
+++ C/SimpleExamples/BubbleSort.c
@@ -5,7 +5,7 @@ void bubble_sort(int *arr, int n)
     //can be implemented in more efficient way
     for (int i = 0; i < n; i++)
     {
-        for(int i = 0; k < n)
+        for(int k = 0; k < n - 1; k++)
 	{
             if(arr[i] < arr[k])
             {
